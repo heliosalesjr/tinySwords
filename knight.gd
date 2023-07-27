d409f9db67963c2f58ca64a0f448d991dc43456a
heliosalesjr <heliosalesjr@gmail.com>
2023-07-27 18:54:58 -0300

    attack hitbox collisions added

--- knight.gd
+++ knight.gd
@@ -1,9 +1,14 @@
 extends CharacterBody2D
 
+@onready var attack_area_collision: CollisionShape2D = get_node("AttackArea/Collision")
+
 @onready var animation: AnimationPlayer = get_node("Animation")
 
 @export var move_speed: float = 256.0
+
 @onready var texture = $Texture
+
+
 
 var can_attack: bool = true
 
@@ -28,16 +33,19 @@ func get_direction() -> Vector2:
 	).normalized()
 	
 func animate() -> void:
+	
 	if velocity.x > 0:
 		texture.flip_h = false
-		
+		attack_area_collision.position.x = 59
 		
 	if velocity.x < 0:
 		texture.flip_h = true
+		attack_area_collision.position.x = -59
 		
-	if velocity > Vector2.ZERO:
+	if velocity != Vector2.ZERO:
 		animation.play("run")
 		return
+		
 	animation.play("idle")
 
 func attack_handler() -> void:
